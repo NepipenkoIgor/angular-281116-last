7abae50f6adfd3d1e33808476529c09b4a6fcb25
Nepipenko Igor <igornepipenko@gmail.com>
2017-01-19 22:28:29 +0200

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,34 +1,96 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed, fakeAsync, inject,tick} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {Location} from '@angular/common';
+import {AppComponent} from './app.component';
+import {HomeComponent} from "./home/home.component";
+import {ChatComponent} from "./chat/chat.component";
+import {routes} from "./app.module";
+import {Router} from "@angular/router";
+import {ProfileComponent} from "./profile/profile.component";
+import {ProfileService} from "./profile/profile.service";
 
-describe('AppComponent', () => {
+import {Observable} from "rxjs/Observable";
+import 'rxjs/add/observable/of'
+class MockService {
+  public getUsers() {
+    return Observable.of([
+      {"firstName": "Игорь", "surname": "Савин", "country": "ru"},
+      {
+        "firstName": "Андрей",
+        "surname": "Иващенко",
+        "photo": "http://i.imgur.com/NktCSrP.png",
+        "country": "ua"
+      }, {
+        "firstName": "Андрей", "surname": "Еременко",
+        "photo": "http://i.imgur.com/OVMrkuw.png", "country": "ru"
+      }
+    ])
+  }
+}
+
+
+
+describe('App: testing component links', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent,
+        ChatComponent,
+        ProfileComponent
       ],
+      providers: [{provide: ProfileService, useClass: MockService}],
+      imports: [
+        RouterTestingModule.withRoutes(routes)
+      ]
     });
-    TestBed.compileComponents();
+
   });
 
-  it('should create the app', async(() => {
+  it('should have router links', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+    expect(fixture.nativeElement.querySelector('.home').textContent).toBe('Home');
+    expect(fixture.nativeElement.querySelector('.chat1').textContent).toBe('Open Chat 1');
+    expect(fixture.nativeElement.querySelector('.chat2').textContent).toBe('Open Chat 2');
+    expect(fixture.nativeElement.querySelector('.lazy').textContent).toBe('Lazy');
+    expect(fixture.nativeElement.querySelector('.profile').textContent).toBe('Profile');
+  });
 
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
+  it('should be able to navigate to lazy by router API', fakeAsync(inject([Router, Location],
+    (router: Router, location: Location) => {
+      TestBed.createComponent(AppComponent);
+      router.navigate(['profile']);
+      tick();
+      expect(location.path()).toBe('/profile')
+      router.navigateByUrl('/profile');
+      tick();
+      expect(location.path()).toBe('/profile')
+    })))
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  }));
+  it('should be able to redirect on no state', fakeAsync(inject([Router, Location],
+    (router: Router, location: Location) => {
+      TestBed.createComponent(AppComponent);
+      router.navigate(['profileasdasdasdsd']);
+      tick();
+      expect(location.path()).toBe('/home')
+      router.navigateByUrl('/home/asdasd/1');
+      tick();
+      expect(location.path()).toBe('/home')
+    })))
+
+  it('should be able to navigate by click', fakeAsync(inject([Router, Location],
+    (router: Router, location: Location) => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+
+      fixture.nativeElement.querySelector('.profile').click();
+      tick();
+      expect(location.path()).toBe('/profile')
+
+      fixture.nativeElement.querySelector('.home').click();
+      tick();
+      expect(location.path()).toBe('/home')
+    })))
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+
+import {RouterModule, Routes} from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileService } from './profile/profile.service';
+
+export const routes: Routes = [
+ // {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'chat1', component: ChatComponent, outlet:"aux1"},
+  {path: 'chat2', component: ChatComponent, outlet:"aux2"},
+  {path: 'lazy', loadChildren: 'app/lazy/lazy.module'},
+  {path: 'profile', component: ProfileComponent},
+  {path: '**', redirectTo: 'home', pathMatch: 'full'},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ChatComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
